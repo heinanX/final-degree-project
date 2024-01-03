aac3387450c3682606d219b6afe9681a89ea7d42
heinanX <larrii@live.se>
2024-01-03 14:51:08 +0100

    user router

---
+++ server/src/resources/orders/ordersRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { createOrderDB, deleteOrder,getOrder, getOrders, manageOrder } from './ordersController';
+import { isAdmin } from '../_middlewares/isAdmin';
+import { validate } from '../_middlewares/validateSchema';
+import { markOrderJoiSchema, orderJoiSchema } from './ordersModel';
+import { formatData } from '../_middlewares/formatData';
+
+export const orderRouter = Router();
+
+orderRouter.get('/', getOrders);
+orderRouter.get('/:id', getOrder);
+orderRouter.post('/create', validate(orderJoiSchema), formatData, createOrderDB);
+orderRouter.put('/manage-order/:id', validate(markOrderJoiSchema), formatData, manageOrder);
+orderRouter.delete('/delete/:id', isAdmin, deleteOrder);

16ff70b6bb713732c31632d167145a58316dd7af
heinanX <larrii@live.se>
2024-01-03 14:50:21 +0100

    order Model edit

--- server/src/resources/orders/ordersModel.ts
+++ server/src/resources/orders/ordersModel.ts
@@ -2,25 +2,33 @@ import { Schema, model, models } from "mongoose";
 import Joi from "joi";
 
 const orderSchema = new Schema({
-  customer: { type: Schema.Types.ObjectId, ref: "customers" },
-  order: [{ type: Schema.Types.ObjectId, ref: "products" }],
-  total_price: { type: Number, default: 0 },
+  customer: { type: Schema.Types.ObjectId, ref: "customers", require: true},
+  order: [{ type: Schema.Types.ObjectId, ref: "products", require: true }],
+  total_price: { type: Number, default: 0, require: true},
   discount: { type: Number, default: 0 },
   date: [{ type: Date, default: Date.now() }],
-  payment_status: { type: String, default: "awaiting" },
   shipped: { type: Boolean, default: "false" },
-  returned: { type: Boolean, default: "false" }
+  returned: { type: Boolean, default: "false" },
+  payment_status: { type: String, default: "pending" },
+  order_status: { type: String, default: "active" }
 }, { versionKey: false });
 
+export const markOrderJoiSchema = Joi.object({
+  shipped: Joi.boolean(),
+  returned: Joi.boolean(),
+  payment_status: Joi.string(),
+  order_status: Joi.string()
+});
 export const orderJoiSchema = Joi.object({
   customer: Joi.string().required(),
   order: Joi.array().items(Joi.string()).required(),
   total_price: Joi.number().required(),
-  discount: Joi.number().required(),
-  date: Joi.date().required(),
-  payment_status: Joi.string().required(),
+  discount: Joi.number(),
+  date: Joi.date(),
   shipped: Joi.boolean(),
-  returned: Joi.boolean()
+  returned: Joi.boolean(),
+  payment_status: Joi.string(),
+  order_status: Joi.string(),
 });
 
 export const OrderModel = models.orders || model("orders", orderSchema);

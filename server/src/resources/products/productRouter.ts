d5d03b184092f784f69eaa74d7161a64ed9ea962
heinanX <larrii@live.se>
2024-01-05 18:04:48 +0100

    product: archive in stripe

--- server/src/resources/products/productRouter.ts
+++ server/src/resources/products/productRouter.ts
@@ -5,11 +5,12 @@ import { validate } from '../_middlewares/validateSchema';
 import { productJoiSchema } from './productModel';
 import { formatData } from '../_middlewares/formatData';
 import { createStripeProduct } from '../_middlewares/stripe/createProduct';
+import { archiveStripeProduct } from '../_middlewares/stripe/archiveProduct';
 
 export const productRouter = Router();
 
 productRouter.get('/', getProducts);
 productRouter.get('/:id', getProduct);
-productRouter.post('/create',  validate(productJoiSchema), formatData, createStripeProduct, createProduct);
+productRouter.post('/create', validate(productJoiSchema), formatData, createStripeProduct, createProduct);
 productRouter.post('/edit-product/:id', isAdmin, validate(productJoiSchema), formatData, editProduct);
-productRouter.delete('/delete/:id', isAdmin, deleteProduct);
+productRouter.delete('/delete/:id', isAdmin, archiveStripeProduct, deleteProduct);

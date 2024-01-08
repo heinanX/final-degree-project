1dabe5ff27f94ef572c479f1d0628daf56738d48
heinanX <larrii@live.se>
2024-01-08 10:11:07 +0100

    start on retro

--- server/dist/resources/products/productRouter.js
+++ server/dist/resources/products/productRouter.js
@@ -12,6 +12,6 @@ const archiveProduct_1 = require("../_middlewares/stripe/archiveProduct");
 exports.productRouter = (0, express_1.Router)();
 exports.productRouter.get('/', productController_1.getProducts);
 exports.productRouter.get('/:id', productController_1.getProduct);
-exports.productRouter.post('/create', (0, validateSchema_1.validate)(productModel_1.productJoiSchema), formatData_1.formatData, createProduct_1.createStripeProduct, productController_1.createProduct);
+exports.productRouter.post('/create', isAdmin_1.isAdmin, (0, validateSchema_1.validate)(productModel_1.productJoiSchema), formatData_1.formatData, createProduct_1.createStripeProduct, productController_1.createProduct);
 exports.productRouter.post('/edit-product/:id', isAdmin_1.isAdmin, (0, validateSchema_1.validate)(productModel_1.productJoiSchema), formatData_1.formatData, productController_1.editProduct);
 exports.productRouter.delete('/delete/:id', isAdmin_1.isAdmin, archiveProduct_1.archiveStripeProduct, productController_1.deleteProduct);
--- server/src/resources/products/productRouter.ts
+++ server/src/resources/products/productRouter.ts
@@ -11,6 +11,6 @@ export const productRouter = Router();
 
 productRouter.get('/', getProducts);
 productRouter.get('/:id', getProduct);
-productRouter.post('/create', validate(productJoiSchema), formatData, createStripeProduct, createProduct);
+productRouter.post('/create', isAdmin, validate(productJoiSchema), formatData, createStripeProduct, createProduct);
 productRouter.post('/edit-product/:id', isAdmin, validate(productJoiSchema), formatData, editProduct);
 productRouter.delete('/delete/:id', isAdmin, archiveStripeProduct, deleteProduct);

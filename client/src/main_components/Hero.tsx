c821fee75991b919fe9bbac48a45aac5751c4c60
heinanX <larrii@live.se>
2024-01-10 00:15:00 +0100

    frontend - tailwind, hero and start on header

--- client/src/main_components/Hero.tsx
+++ client/src/main_components/Hero.tsx
@@ -1,7 +1,66 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import { useEffect, useState } from "react";
+import hero_1 from "../assets/images/ai-generated-hero.png";
+import hero_2 from "../assets/images/ai-generated-hero_2.png";
+import hero_4 from "../assets/images/ai-generated-hero_4.png";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-export default Hero
+const Hero = () => {
+  const images = [hero_1, hero_2, hero_4];
+  const [num, setNum] = useState(0);
+  const [heroImage, setHeroImage] = useState(images[num]);
+
+
+  const forward = () => {
+    if (num >= images.length - 1) {
+      setNum(0);
+    } else {
+      setNum(num + 1);
+    }
+    setHeroImage(images[num + 1] || images[0]);
+  };
+
+  const backwards = () => {
+    if (num === 0) {
+      setNum(images.length - 1);
+    } else {
+      setNum(num - 1);
+    }
+    setHeroImage(images[num - 1] || images[images.length - 1]);
+  };
+
+  function createDots() {
+    return images.map((item, index) => (
+      <div key={index} className={ `w-2 h-2  rounded-full ${num === index ? 'bg-cyan-700' : 'bg-white'}` } ></div>
+    ));
+  }
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      forward();
+    }, 8000);
+  
+    return () => clearInterval(intervalId);
+  }, [num]);
+
+  const arrowStyling =
+    "absolute bottom-1/2 text-white hover:text-cyan-400 text-4xl drop-shadow-xl opacity-80";
+
+  return (
+    <div
+      className="relative w-full h-533"
+      style={{ backgroundImage: `url(${heroImage})`, height: "533px" }}
+    >
+      <button onClick={backwards} className={`${arrowStyling} left-10`}>
+        {" "}
+        <FaArrowLeft />{" "}
+      </button>
+      <button onClick={forward} className={`${arrowStyling} right-10`}>
+        {" "}
+        <FaArrowRight />{" "}
+      </button>
+      <div className="absolute bottom-6 right-1/2 flex gap-2">{createDots()}</div>
+    </div>
+  );
+};
+
+export default Hero;
--- client/src/main_components/Router.tsx
+++ client/src/main_components/Router.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import CartPage from "../pages/CartPage";
+import LoginPage from "../pages/LoginPage";
+import CustomerPage from "../pages/CustomerPage";
 
 const Router = () => {
   return (
-    <main>
+    <main className=" w-screen">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
       <Route path="/cart" element={<CartPage />} />
-      <Route path="/account" element={<AccountPage />} /> */}
+      <Route path="/account" element={<CustomerPage />} />
       </Routes>
     </main>
   );

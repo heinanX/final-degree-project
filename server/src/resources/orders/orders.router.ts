911105f5fa86692f3bc9623e125e0528e19bc984
heinanX <larrii@live.se>
2024-01-21 23:58:50 +0100

    order router

--- server/src/resources/orders/orders.router.ts
+++ server/src/resources/orders/orders.router.ts
@@ -13,7 +13,7 @@ export const orderRouter = Router();
 orderRouter.get('/', isAdmin, getOrders);
 orderRouter.get('/user-orders/:id', authorization, getOrders);
 orderRouter.get('/:id', getOrder);
-orderRouter.post('/create', authenticateLogin,checkOrderStatus, validate(orderJoiSchema), createOrderDB);
+orderRouter.post('/create', authenticateLogin, checkOrderStatus, validate(orderJoiSchema), createOrderDB);
 orderRouter.post('/create-checkout-session', authenticateLogin, createCheckoutSession)
 orderRouter.put('/manage-order/:id', validate(markOrderJoiSchema), formatData, manageOrder);
 orderRouter.delete('/delete/:id', isAdmin, deleteOrder);

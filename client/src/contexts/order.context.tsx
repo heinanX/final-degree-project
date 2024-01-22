ef4c23bc686bc4fdb65c5d6b647b183e4b73c6f2
heinanX <larrii@live.se>
2024-01-22 03:51:26 +0100

    client: order.context update

--- client/src/contexts/order.context.tsx
+++ client/src/contexts/order.context.tsx
@@ -29,47 +29,40 @@ function OrderProvider({ children }: PropsWithChildren) {
   const { cartTotal } = cartSocket();
 
   const createOrderDatabase = async (cart: CartItem[], sessionId: string) => {
-    
-    const itemsInCart = cart.map(item => {
+    const itemsInCart = cart.map((item) => {
       const { product, quantity, digital, vhs } = item;
       const newObject = {
-          product: product._id,
-          quantity: quantity,
-          digital: digital,
-          vhs: vhs
-      }
+        product: product._id,
+        quantity: quantity,
+        digital: digital,
+        vhs: vhs,
+      };
       return newObject;
     });
 
-    const once = {
-      session_id: sessionId,
-      order: itemsInCart,
-      total_price: cartTotal
-    }
-    console.log('cons', once);
-    
-    
     try {
       const res = await fetch("/api/orders/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(once),
+        body: JSON.stringify({
+          session_id: sessionId,
+          order: itemsInCart,
+          total_price: cartTotal,
+        }),
       });
       const data = await res.json();
       if (res.ok) {
         console.log(data);
-        setOrder(data)
+        setOrder(data);
       }
     } catch (err) {
       // Handle errors, if any
       if (err instanceof Error) {
         console.error("Error fetching product", err.message);
       }
-      
     }
-    
   };
 
   return (
